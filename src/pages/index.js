68b1a7ba2f791a51c8dab456cd98a599c1b05825
darinetag <tagdarinefatma@gmail.com>
2025-05-14 12:10:39 +0100

    table done yay

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,46 +22,47 @@ export default function Home() {
       </section>
             <section>
               <h1 style={{ fontFamily: 'Science' }} className="font-science text-8xl text-[#0E2A8B] flex items-center justify-center ">Skills</h1> </section>
-  <section className="p-4 bg-black ">
-  <div className="grid grid-cols-12 gap-2 ">
+  <section className="p-2 bg-black mt-20 ">
+  <div className="grid grid-cols-12 gap-2  text-2xl text-center " style={{ fontFamily: 'cormorant' }}>
     {/* First row */}
-    <div className="col-span-3 bg-white text-black p-2 hover:bg-white transition duration-300 ease-in-out">Web Design</div>
-    <div className="col-span-6 bg-black text-white p-2 hover:bg-white hover:text-black transition duration-300 ease-in-out">HTML CSS Javascript GitHub Git</div>
-    <div className="col-span-3 bg-yellow-400 text-black p-2 hover:bg-white transition duration-300 ease-in-out">Next.js</div>
+<div className="col-span-3 bg-white text-black p-2 flex items-center justify-center hover:bg-white transition duration-300 ease-in-out">Web Design</div>
+    <div className="col-span-6 h-14 bg-black text-white p-2 flex items-center justify-center hover:bg-white hover:text-black transition duration-300 ease-in-out">HTML CSS Javascript GitHub Git</div>
+    <div className="col-span-3 h-14 bg-yellow-400 text-black p-2 flex items-center justify-center hover:bg-white transition duration-300 ease-in-out">Next.js</div>
 
     {/* Second row */}
-    <div className="col-span-1 bg-red-700 p-2 hover:bg-white transition duration-300 ease-in-out"></div>
-    <div className="col-span-2 bg-white text-black p-2 hover:bg-white transition duration-300 ease-in-out">Visual Design</div>
-    <div className="col-span-3 bg-blue-900 text-white p-2 hover:bg-white hover:text-black transition duration-300 ease-in-out">Figma</div>
-    <div className="col-span-3 bg-red-700 text-white p-2 hover:bg-white hover:text-black transition duration-300 ease-in-out">Translation</div>
-    <div className="col-span-3 bg-white text-black p-2 hover:bg-white transition duration-300 ease-in-out">WordPress</div>
+    <div className="col-span-1 h-42 bg-red-700 p-2 hover:bg-white transition duration-300 ease-in-out"></div>
+    <div className="col-span-2 h-42 bg-white text-black p-2 flex items-center justify-center hover:bg-white transition duration-300 ease-in-out">Visual Design</div>
+    <div className="col-span-3 h-42 bg-blue-900 text-white p-2 flex items-center justify-center hover:bg-white hover:text-black transition duration-300 ease-in-out">Figma</div>
+    <div className="col-span-3 h-42 bg-red-700 text-black p-2 flex items-center justify-center hover:bg-white hover:text-black transition duration-300 ease-in-out">Translation</div>
+    <div className="col-span-3 h-42 bg-white text-black p-2 flex items-center justify-center hover:bg-white transition duration-300 ease-in-out">WordPress</div>
 
     {/* Third row */}
-    <div className="col-span-2"></div>
-    <div className="col-span-2 bg-yellow-400 text-black p-2 hover:bg-white transition duration-300 ease-in-out">UX Writing</div>
-    <div className="col-span-3 bg-red-700 text-white p-2 hover:bg-white hover:text-black transition duration-300 ease-in-out">Proofreading</div>
-    <div className="col-span-3 bg-white text-black p-2 hover:bg-white transition duration-300 ease-in-out">Framer</div>
+    <div className="col-span-2 hover:bg-white transition duration-300 ease-in-out"></div>
+    <div className="col-span-2 h-14 bg-yellow-400 text-black p-2 flex items-center justify-center hover:bg-white transition duration-300 ease-in-out">UX Writing</div>
+    <div className="col-span-3 h-14 bg-red-700 text-black p-2 flex items-center justify-center hover:bg-white hover:text-black transition duration-300 ease-in-out">Proofreading</div>
+    <div className="col-span-3 h-14 bg-white text-black p-2 flex items-center justify-center hover:bg-white transition duration-300 ease-in-out">Framer</div>
 
     {/* Fourth row */}
-    <div className="col-span-2 bg-blue-900 text-white p-2 hover:bg-white hover:text-black transition duration-300 ease-in-out">Typography</div>
-    <div className="col-span-2 bg-red-700 text-white p-2 hover:bg-white hover:text-black transition duration-300 ease-in-out">Color Theory</div>
-    <div className="col-span-2 bg-white text-black p-2 hover:bg-white transition duration-300 ease-in-out">Layout</div>
-    <div className="col-span-1 bg-blue-900 p-2 hover:bg-white transition duration-300 ease-in-out"></div>
-    <div className="col-span-3 bg-white text-black p-2 hover:bg-white transition duration-300 ease-in-out">User Research</div>
-    <div className="col-span-2 bg-white p-2 hover:bg-white transition duration-300 ease-in-out"> 
-      <img src="/figma-icon.svg" alt="Figma" className="w-6 h-6 mx-auto" />
+    <div className="col-span-2 h-14 bg-blue-900 text-white p-2 flex items-center justify-center hover:bg-white hover:text-black transition duration-300 ease-in-out">Typography</div>
+    <div className="col-span-2 h-14 bg-red-700 text-black p-2 flex items-center justify-center hover:bg-white hover:text-black transition duration-300 ease-in-out">Color Theory</div>
+    <div className="col-span-2 h-14 bg-white text-black p-2 flex items-center justify-center hover:bg-white transition duration-300 ease-in-out">Layout</div>
+    <div className="col-span-1 h-14 bg-blue-900 p-2 flex items-center justify-center hover:bg-white transition duration-300 ease-in-out"></div>
+    <div className="col-span-3 h-14 bg-white text-black p-2 flex items-center justify-center hover:bg-white transition duration-300 ease-in-out">User Research</div>
+    <div className="col-span-2 h-14 bg-yellow-400 p-2 flex items-center justify-center hover:bg-white transition duration-300 ease-in-out"> 
+      <img src="/figma-icon.png" alt="Figma" className="w-6 h-8 mx-auto flex items-center justify-center" />
     </div>
+    <div className="col-span-2 hover:bg-white transition duration-300 ease-in-out"></div>
 
     {/* Fifth row */}
-    <div className="col-span-3 bg-yellow-400 text-black p-2 hover:bg-white transition duration-300 ease-in-out">UI/UX Principles</div>
-    <div className="col-span-1 bg-yellow-400 p-2 hover:bg-white transition duration-300 ease-in-out">
-      <img src="/git-icon.svg" alt="Git" className="w-6 h-6 mx-auto" />
+    <div className="col-span-3 h-20 bg-yellow-400 text-black p-2 flex items-center justify-center hover:bg-white transition duration-300 ease-in-out">UI/UX Principles</div>
+    <div className="col-span-1 h-20 bg-yellow-400 p-2 hover:bg-white transition duration-300 ease-in-out">
+      <img src="/git-icon.png" alt="Git" className="w-8 h-8 mx-auto flex items-center justify-center mt-4" />
     </div>
-    <div className="col-span-2 bg-white text-black p-2 hover:bg-white transition duration-300 ease-in-out">AI</div>
-    <div className="col-span-1 bg-red-700 p-2 hover:bg-white transition duration-300 ease-in-out">
-      <img src="/github-icon.svg" alt="GitHub" className="w-6 h-6 mx-auto" />
+    <div className="col-span-2 h-20 bg-white text-black p-2 flex items-center justify-center hover:bg-white transition duration-300 ease-in-out">AI</div>
+    <div className="col-span-1 h-20 bg-red-700 p-2 flex items-center justify-center hover:bg-white transition duration-300 ease-in-out">
+      <img src="/github-icon.png" alt="GitHub" className="w-6 h-6 mx-auto" />
     </div>
-    <div className="col-span-5 bg-white text-black p-2 hover:bg-white transition duration-300 ease-in-out">Mockups and Prototyping</div>
+    <div className="col-span-5 h-20 bg-blue-900 text-white p-2 flex items-center justify-center hover:bg-white hover:text-black transition duration-300 ease-in-out">Mockups and Prototyping</div>
   </div>
 </section>
 
