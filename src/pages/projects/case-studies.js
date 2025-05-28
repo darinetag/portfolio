063ccd8ab00c95e29389209ae9dbecb93e2f009b
darinetag <tagdarinefatma@gmail.com>
2025-05-28 12:52:42 +0100

    case studies page, 2 done

--- src/pages/projects/case-studies.js
+++ src/pages/projects/case-studies.js
@@ -4,14 +4,16 @@ import ProjectExample from "@/components/ProjectExample";
 export default function ChatgptFeatured() {
   const examples = [
     {
-      heading: "Featured Chats",
-      description: "As someone who carefully prompts ChatGPT for different tasks, I often revisit the same 3–4 conversations. But finding them? isn't easy, I would still need to scroll through all the chats. So I asked myself: What if we could “feature” chats the same way we pin messages or star notes?",
-      image: "/projects/chagpt-featured.png",
+      heading:  "Search Functionality",
+            description: "On Nov 14, I suggested adding a search feature to ChatGPT to find past conversations more easily. By Dec 4, it was live. No reply from the team, but seeing the idea implemented felt like a small UX win — proof that I’m thinking like a designer spotting the gap before it was filled",
+      image: "/projects/search.png",
+
     },
     {
-      heading: "Improved Pin System",
-      description: "Introduced ability to pin and reorder chats.",
-      image: "/projects/chatgpt-example2.png",
+      heading: "Featured Chats",
+      description: "As someone who carefully prompts ChatGPT for different tasks, I often revisit the same 3–4 conversations. But finding them? isn't easy, I would still need to scroll through all the chats. So I asked myself: What if we could “feature” chats the same way we pin messages or star notes?",
+            image: "/projects/chagpt-featured.png",
+
     },
     {
       heading: "Final Visual Style",
