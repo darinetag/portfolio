f1b89fe4c32527f11a6a97bd0fd4c03f7bba70ff
darinetag <tagdarinefatma@gmail.com>
2025-06-03 00:44:50 +0100

    responsive hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,14 +10,14 @@ export default function Home() {
       </Head>
       <section className="relative h-screen">
         
-<img src="/darinemotion.gif" alt="My Image" className="w-150 h-140 rounded-lg absolute top-10 right-10 z-10" />
-        <h1 style={{ fontFamily: 'Science' }} className="font-science text-8xl text-[#0E2A8B] absolute top-55 left-8">Darine Tag</h1>
+<img src="/darinemotion.gif" alt="My Image" className="lg:w-150 lg:h-140 lg:left-230 lg:top-10 left-160 top-58 w-100 h-90 rounded-lg absolute z-10" />
+        <h1 style={{ fontFamily: 'Science' }} className="font-science lg:text-8xl text-6xl text-[#0E2A8B] absolute lg:top-55 top-63 left-8">Darine Tag</h1>
        
-<div className="w-76 h-76 bg-[#BC0700] border-10 border-black absolute top-0 right-0 hover:bg-white transition duration-300 ease-in-out"></div>
-<div className="w-24 h-52 bg-[#0E2A8B] border-10 border-black absolute top-74 right-0 hover:bg-white transition duration-300 ease-in-out"></div>
+<div className="lg:w-76 lg:h-76 w-40 h-40 top-50 bg-[#BC0700] border-10 border-black absolute lg:top-0 lg:left-305 left-216 hover:bg-white transition duration-300 ease-in-out"></div>
+<div className="lg:w-24 lg:h-52 hidden lg:block bg-[#0E2A8B] border-10 border-black absolute top-74 right-0 hover:bg-white transition duration-300 ease-in-out"></div>
 <div style={{ fontFamily: 'Montreal' }} className="w-256 h-65 bg-[#FBC408] border-10 border-black absolute top-85 left-0 hover:bg-white transition duration-300 ease-in-out"> 
    <h1  className="pl-6 pt-7 text-2xl">Making Tech Kind</h1>
-   <p  className="pl-5 p-4 text-xl w-230">As someone who is very much passionate about learning, I have managed to gather a good amount of skillsets that not only allowed me to be versatile and a quick problem solver but moreover it helped me become extremely adaptable to new things and a good communicator with people from different backgrounds. I bring creativity and appeal to digital solutions. </p>
+   <p  className="pl-5 p-4 lg:text-xl lg:w-230 w-160 text-md">As someone who is very much passionate about learning, I have managed to gather a good amount of skillsets that not only allowed me to be versatile and a quick problem solver but moreover it helped me become extremely adaptable to new things and a good communicator with people from different backgrounds. I bring creativity and appeal to digital solutions. </p>
 </div>
       </section>
             <section>
