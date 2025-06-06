7a2accc679415611e2b55bdea194ed401ee86ec7
darinetag <tagdarinefatma@gmail.com>
2025-06-06 02:10:11 +0100

    updated favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+       
+         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+  <link rel="shortcut icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <body className="antialiased">
